e84a1e3bfcb80cfe30635277505fe7c59315d987
Alex Kochutov <alex.kochutov@gmail.com>
2022-10-31 03:16:56 +0400

    feat: Include tests for Post response

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldPostAnotherData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("another data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("another data"))
+        ;
+    }
+}
